f21753b1730b31b2af53ae47ac5e095f36522dd2
Isaya Opiyo <isayahopiyo807@gmail.com>
2023-07-05 08:48:10 +0300

    step -2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:notesapp/views/login_view.dart';
+import 'package:notesapp/views/register_view.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -7,13 +9,17 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
+        routes: {
+          //the value of every key inside the routes needs to be a function
+          '/login/': (context) => const LoginView(),
+          '/register/': (context) => const RegisterView(),
+        }),
   );
 }
 
@@ -22,30 +28,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              // If the value on the lefhandside exists take it otherwiseR return false
-              if (user?.emailVerified ?? false) {
-                print("You are a verified user");
-              } else {
-                print("You need to be verified first");
-              }
-              return const Text('Done');
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            //final user = FirebaseAuth.instance.currentUser;
+            // If the value on the lefhandside exists take it otherwiseR return false
+            // if (user?.emailVerified ?? false) {
+            //  return const Text('Done');
+            //} else {
+            //  return const VerifyEmailView();
+            // }
+            return const LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
