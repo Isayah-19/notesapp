fee1d2c01128694465181b64d3081c90b0ce2fc5
Isaya Opiyo <isayahopiyo807@gmail.com>
2023-07-05 08:57:28 +0300

    step -2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:notesapp/views/login_view.dart';
 import 'package:notesapp/views/register_view.dart';
+import 'package:notesapp/views/verify_email_view.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -35,14 +37,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            //final user = FirebaseAuth.instance.currentUser;
-            // If the value on the lefhandside exists take it otherwiseR return false
-            // if (user?.emailVerified ?? false) {
-            //  return const Text('Done');
-            //} else {
-            //  return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
