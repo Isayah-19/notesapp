e2da0171bd9f0334a282a08c51310b3aafd7671b
Isaya Opiyo <isayahopiyo807@gmail.com>
2023-07-25 15:58:56 +0300

    step -17

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:notesapp/constants/routes.dart';
 import 'package:notesapp/services/auth/auth_service.dart';
 
 import 'package:notesapp/views/login_view.dart';
-import 'package:notesapp/views/notes/new_note_view.dart';
+import 'package:notesapp/views/notes/create_update_note_view.dart';
 import 'package:notesapp/views/notes/notes_view.dart';
 import 'package:notesapp/views/register_view.dart';
 import 'package:notesapp/views/verify_email_view.dart';
@@ -25,7 +25,7 @@ void main() {
           registerRoute: (context) => const RegisterView(),
           notesRoute: (context) => const NotesView(),
           verifyEmailRoute: (context) => const VerifyEmailView(),
-          newNoteRoute: (context) => const NewNoteView(),
+          createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
         }),
   );
 }
